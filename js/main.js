c34aab77339144b8ef05d23fa372d0fc779f8b30
byeweb2 <sy47525@gmail.com>
2022-09-03 22:34:33 +0100

    Talk wiht chrome API to save URL in the current Tap and add Permissionin tab to our json file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-// Grap the save button element
+// Grap the save input button element
 const saveLead = document.getElementById('save-btn');
+// Grap the save tab button element
+const saveTab = document.getElementById('save-tab');
 // Grap the delete button element
 const deleteLead = document.getElementById('delete-btn');
 // Grap The input element
@@ -16,6 +18,17 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads);
 }
+
+// Add click listen to save tab button
+saveTab.addEventListener('click', function () {
+    // Talk with chrome API to ask make a query in the current window and tab to fetch the URL
+        // Then store in our url list, localstorage too and display it
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
+});
 
 // Create double click event 
 deleteLead.addEventListener('dblclick', function () {
